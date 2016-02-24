3440a32d2c2d1641a3959ac41b621e56d73f07e8
CedricBm <beaumont.cedric@gmail.com>
2016-02-24 15:28:01 +0100

    Connects Hibernate

--- src/fr/papyfinance/com/servlets/AccueilServlet.java
+++ src/fr/papyfinance/com/servlets/AccueilServlet.java
@@ -1,42 +1,37 @@
 package fr.papyfinance.com.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class AccueilServlet
- */
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import fr.papyfinance.com.beans.Company;
+import fr.papyfinance.com.beans.HibernateUtil;
+
 @WebServlet("/")
 public class AccueilServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AccueilServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		System.out.println("======================coucou");
+		Company c = new Company();
+		c.setName("test company");
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        session.save(c);
+        session.getTransaction().commit();
+        sessionFactory.close();
+        
 		this.getServletContext().getRequestDispatcher( "/WEB-INF/accueil.jsp" ).forward( request, response );
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
 }
